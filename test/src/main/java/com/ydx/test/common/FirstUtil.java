0492dbd101da6d98f6f501e67b29f3d301004171
后来 <@LAPTOP-7NJ1MLTA.lan>
2018-07-12 19:23:37 +0800

    第二次提交

--- test/src/main/java/com/ydx/test/common/FirstUtil.java
+++ test/src/main/java/com/ydx/test/common/FirstUtil.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 public class FirstUtil {
     public static String createId() {
     	//12121212
+    	//今天是2018年7月12日
         return UUID.randomUUID().toString().replaceAll("-","");
     }
 }
